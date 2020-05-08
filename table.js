0e6d327cdebfc5cbbe95951bdcc56cc910b1c816
Cristian Bohorquez <cristian.bohorquez@inoutdelivery.com>
2020-05-07 19:15:49 -0500

    connection

--- table.js
+++ table.js
@@ -25,6 +25,26 @@ $(document).ready(function () {
                 table_data += '</table>';
                 $('#employee_table').html(table_data);
                 // Push a base de datos
+                
+                var mysql = require('mysql');
+
+                var con = mysql.createConnection({
+                    host: "localhost",
+                    user: "myusername",
+                    password: "mypassword",
+                    database: "mydb"
+                    });
+
+                con.connect(function(err) {
+                    console.log('🖐', err)
+                if (err) throw err;
+                //Select all customers and return the result object:
+                con.query("SELECT * FROM customers", function (err, result, fields) {
+                    if (err) throw err;
+                    console.log(result);
+                });
+              });
+             
             }
         });
     });
